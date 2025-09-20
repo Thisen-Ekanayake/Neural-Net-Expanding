bc86429bcc77ad5627cd3dbd017c92cabe280d08
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-09-20 22:47:52 +0530

    cnn model

---
+++ cnn/classification_binary/binary_cnn_model.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(128*28*28, 256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256,1)    # binary output
+        )
+    
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
